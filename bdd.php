55ce855412d109d1ae1c0e23d2a24d7480725ee0
Alexandre Bulcao <ambulcao@gmail.com>
2024-05-09 17:35:40 +0100

    primeira versão do projeto
    inicialmente foi criado o ficheiro bdd para conexão com o banco de dados, um index para que seja recebido as informações do user e um ficheiro style.

---
+++ bdd.php
@@ -0,0 +1,17 @@
+<?php 
+
+$dbHost = "localhost";
+$dbName = "optigest"; 
+$dbUser = "root";  
+$dbPassword = " "; 
+
+try {
+    $db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Conexão com o banco de dados Optigest estabelecida com sucesso!";
+} catch (PDOException $e) {
+    echo "Erro ao conectar com o banco de dados Optigest: " . $e->getMessage();
+}
+
+$db = null;
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Gerenciamento de Funcionários e Projetos</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <div class="container centralizar-pagina">
+        <h1 class="conteudo">Gerenciamento de Funcionários e Projetos</h1>
+        <ul class="nav nav-pills conteudo">
+            <li class="nav-item"><a href="employees.php" class="nav-link">Cadastrar Funcionário</a></li>
+            <li class="nav-item"><a href="projects.php" class="nav-link">Cadastrar Projeto</a></li>
+            <li class="nav-item"><a href="allocation.php" class="nav-link">Alocar Funcionário em Projeto</a></li>
+        </ul>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
